d9a93ac9440493d1947a47df778c177da329d112
Boston Stamm <104592830+BostonStamm@users.noreply.github.com>
2024-06-02 00:04:23 -0600

    W06

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,3 +1,5 @@
+using System.Runtime.CompilerServices;
+
 public class Node {
     public int Data { get; set; }
     public Node? Right { get; private set; }
@@ -8,7 +10,9 @@ public class Node {
     }
 
     public void Insert(int value) {
-        if (value < Data) {
+        if (value == Data){
+            
+        } else if (value < Data) {
             // Insert to the left
             if (Left is null)
                 Left = new Node(value);
@@ -26,11 +30,37 @@ public class Node {
 
     public bool Contains(int value) {
         // TODO Start Problem 2
+        if(Data == value){
+            return true;
+        }else if(Data < value){
+            if(Right is null){
+                return false;
+            }else if(Right.Contains(value)){
+                return true;
+            }
+        }else{
+            if(Left is null){
+                return false;
+            }else if(Left.Contains(value)){
+                return true;
+            }
+        }
         return false;
     }
 
     public int GetHeight() {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        // Replace this line with the correct return statement(s)
+
+        int left = 1;
+        int right = 1;
+        if(Left is not null){
+            left = Left.GetHeight()+1;
+        }
+        if(Right is not null){
+            right = Right.GetHeight()+1;
+        }
+
+        return(Math.Max(left, right));
     }
 }
